59d0c817381cf7ff3f6c77351cb2676dfb030407
marzkup54 <marzenakupka@tlen.pl>
2021-12-02 14:20:01 +0100

    Dodaj pliki projektów.

---
+++ BindingProduct 3p/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingProduct_3p
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Product pr1 = new Product("ax111", "klawiatura", 15, "glowny");
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Product pr1 = new Product("ax111", "klawiatura", 15, "glowny");
+
+            GridProducts.DataContext = pr1;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string tekst =
+                String.Format("{0}{1}{2}", "Wprowadzono dane:",
+                                            Environment.NewLine, pr1.ToString());
+            MessageBox.Show(tekst,"produkt",MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DateTime data = DateTime.Now; // Odczytanie bieżącej daty
+            btn_czas.Content = data.ToString("f");
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btn_czas.Content = "Godzina";
+        }
+    }
+}
---
+++ BindingProduct 3p/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingProduct_3p
+{
+    internal class Product
+    {
+        public string Symbol { get; set; }
+        public string Nazwa { get; set; }
+        public int LiczbaSztuk{ get; set; }
+        public string Magazyn { get; set; }
+
+        public Product(string symbol, string nazwa, int liczbaSztuk, string magazyn)
+        {
+            Symbol = symbol;
+            Nazwa = nazwa;
+            LiczbaSztuk = liczbaSztuk;
+            Magazyn = magazyn;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}, {3}", Symbol, Nazwa, LiczbaSztuk,
+            Magazyn);
+        }
+
+
+    }
+}
